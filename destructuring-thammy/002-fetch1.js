8bf98e874676ac1714ef6716e7b4afe04806f5da
AmandaCacholi <amanda.cacholi@outlook.com>
2021-02-12 23:17:49 -0300

    edição

--- destructuring-thammy/002-fetch1.js
+++ destructuring-thammy/002-fetch1.js
@@ -9,3 +9,4 @@ fetch('https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772')
     console.log(`ingredients: ${data.meals[0].strIngredient1}, ${data.meals[0].strIngredient2}, ${data.meals[0].strIngredient3}, ${data.meals[0].strIngredient4}, ${data.meals[0].strIngredient5}, ${data.meals[0].strIngredient6}, ${data.meals[0].strIngredient7}, ${data.meals[0].strIngredient8} and ${data.meals[0].strIngredient9}`)
     console.log(`Instructions: ${data.meals[0].strInstructions}`)
 })
+
